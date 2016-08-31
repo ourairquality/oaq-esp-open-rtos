9800774fb81ab82a3f589439366b947a53518979
Our Air Quality <info@ourairquality.org>
2016-08-31 13:17:56 +1000

    Moved the gpio16 supporting in the esp-open-rtos core.

--- leds.c
+++ leds.c
@@ -21,52 +21,17 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include "esp/rtc_regs.h"
+#include "esp/gpio.h"
 #include "config.h"
-
-void gpio16_enable_output()
-{
-    RTC.GPIO_CFG[3] = (RTC.GPIO_CFG[3] & 0xffffffbc) | 1;
-    RTC.GPIO_CONF = (RTC.GPIO_CONF & 0xfffffffe) | 0;
-    RTC.GPIO_ENABLE = (RTC.GPIO_OUT & 0xfffffffe) | 1;
-}
-
-void gpio16_write(uint8_t value)
-{
-    RTC.GPIO_OUT = (RTC.GPIO_OUT & 0xfffffffe) | (value & 1);
-}
-
-void init_blink()
-{
-    switch (param_board) {
-      case 0:
-          gpio16_enable_output();
-          gpio16_write(1);
-          break;
-
-      case 1:
-          /*
-           * Witty param_board setup.
-           * Multi-color LED is on pins 12 13 and 15.
-           */
-          gpio_enable(12, GPIO_OUTPUT); // Green
-          gpio_enable(13, GPIO_OUTPUT); // Blue
-          gpio_enable(15, GPIO_OUTPUT); // Red
-          gpio_write(12, 0);
-          gpio_write(13, 0);
-          gpio_write(15, 0);
-          break;
-    }
-}
 
 void blink_green()
 {
     switch (param_board) {
       case 0:
           // Nodemcu
-          gpio16_write(0);
+          gpio_write(16, 0);
           taskYIELD();
-          gpio16_write(1);
+          gpio_write(16, 1);
           break;
       case 1 :
           // Witty Green
@@ -82,9 +47,9 @@ void blink_blue()
     switch (param_board) {
       case 0:
           // Nodemcu
-          gpio16_write(0);
+          gpio_write(16, 0);
           taskYIELD();
-          gpio16_write(1);
+          gpio_write(16, 1);
           break;
       case 1:
           // Witty Blue
@@ -100,9 +65,9 @@ void blink_red()
     switch (param_board) {
       case 0:
           // Nodemcu
-          gpio16_write(0);
+          gpio_write(16, 0);
           taskYIELD();
-          gpio16_write(1);
+          gpio_write(16, 1);
           break;
 
       case 1:
@@ -119,9 +84,9 @@ void blink_white()
     switch (param_board) {
       case 0:
           // Nodemcu.
-          gpio16_write(0);
+          gpio_write(16, 0);
           taskYIELD();
-          gpio16_write(1);
+          gpio_write(16, 1);
           break;
 
       case 1:
